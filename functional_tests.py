eb5550099fb407ce425a4f200e8f4ac86b490070
root <root@mars.macrotemporal.com>
2020-02-19 19:58:56 -0800

    functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 options = Options()
@@ -15,21 +16,38 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Begin by checking out the homepage
-            self.browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000')
 
         # Notice that the page title and header mention a to-do list.
-            self.assertIn('To-Do', self.browser.title)
-            self.fail('Finish the test!')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Invited to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Enters a to-do list item into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # After entering, the page udpates and the page lists 
         # the item in the list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_elements_by_id('id_list_table')
+        rows = tables.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
 
         # The text box to enter new items is still present. User
         # enters new item
+        self.fail('finish the test')
 
         # Page updates again showing both to-do list items
 
